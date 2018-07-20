12bdda55516fa1cc849f7d46ea35f2f21a572343
svilchez0 <samuel_vilchez@yahoo.com>
2018-07-20 00:45:15 -0700

    More modifications

--- QtProject/Deadpool/seconddialog.cpp
+++ QtProject/Deadpool/seconddialog.cpp
@@ -8,6 +8,51 @@ SecondDialog::SecondDialog(QWidget *parent) :
 {
 //    renderArea = new RenderArea;
     ui->setupUi(this);
+
+    ui->penWidthSpinBox->setMinimum(0);
+    ui->penWidthSpinBox->setMaximum(20);
+
+    ui->penStyleComboBox->addItem("NoPen");
+    ui->penStyleComboBox->addItem("SolidLine");
+    ui->penStyleComboBox->addItem("DashLine");
+    ui->penStyleComboBox->addItem("DotLine");
+    ui->penStyleComboBox->addItem("DashDotLine");
+    ui->penStyleComboBox->addItem("DashDotDotLine");
+
+    ui->penCapComboBox->addItem("FlatCap");
+    ui->penCapComboBox->addItem("SquareCap");
+    ui->penCapComboBox->addItem("RoundCap");
+
+    ui->penJoinComboBox->addItem("MiterJoin");
+    ui->penJoinComboBox->addItem("BevelJoin");
+    ui->penJoinComboBox->addItem("RoundJoin");
+
+    ui->penColorComboBox->addItem("White");
+    ui->penColorComboBox->addItem("Black");
+    ui->penColorComboBox->addItem("Red");
+    ui->penColorComboBox->addItem("Green");
+    ui->penColorComboBox->addItem("Blue");
+    ui->penColorComboBox->addItem("Cyan");
+    ui->penColorComboBox->addItem("Magenta");
+    ui->penColorComboBox->addItem("Yellow");
+    ui->penColorComboBox->addItem("Gray");
+
+    ui->brushColorComboBox->addItem("White");
+    ui->brushColorComboBox->addItem("Black");
+    ui->brushColorComboBox->addItem("Red");
+    ui->brushColorComboBox->addItem("Green");
+    ui->brushColorComboBox->addItem("Blue");
+    ui->brushColorComboBox->addItem("Cyan");
+    ui->brushColorComboBox->addItem("Magenta");
+    ui->brushColorComboBox->addItem("Yellow");
+    ui->brushColorComboBox->addItem("Gray");
+
+
+    ui->brushStyleComboBox->addItem("SolidPattern");
+    ui->brushStyleComboBox->addItem("HorPattern");
+    ui->brushStyleComboBox->addItem("VerPattern");
+    ui->brushStyleComboBox->addItem("NoBrush");
+
 }
 
 SecondDialog::~SecondDialog()
